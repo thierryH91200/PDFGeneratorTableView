ff243cfb400ffe9ca572c01e15311c67dfbafb84
thierryH91200 <thierry.hentic@gmail.com>
2018-09-02 11:16:06 +0200

    Initial Commit

---
+++ PDFGeneratorTableView/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  PDFGeneratorTableView
+//
+//  Created by thierryH24 on 02/09/2018.
+//  Copyright © 2018 thierryH24. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
